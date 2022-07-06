a80107b14f59afb23d92789d771b8eee6fc60394
lulu <caolulu000@gmail.com>
2022-07-06 16:51:48 -0500

    configured dotenv

--- twilio.js
+++ twilio.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
